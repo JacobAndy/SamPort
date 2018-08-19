c6bc3c4a293bf5beb4d28ca0d7ac2f32a8e0406e
Jacob Anderson <odistiinct@gmail.com>
2018-08-18 19:15:04 -0500

    COMMENTS

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-
+{
+  /*
+    unfinished routes, place holders for components
+    */
+}
 export default (
   <Switch>
+    {/*
+    each product
+    */}
     <Route
       exact
       path="/products/:id"
       component={() => <h1>Products Page ID</h1>}
     />
+
     <Route path="/products" component={() => <h1>Products Page</h1>} />
 
     <Route path="/contact" component={() => <h1>Contact Page</h1>} />
