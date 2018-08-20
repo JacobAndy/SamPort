d41d50d208c02510f679642829fce69950a4a944
Jacob Anderson <odistiinct@gmail.com>
2018-08-19 20:20:21 -0500

    folders

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-{
-  /*
-    unfinished routes, place holders for components
-    */
-}
+
 export default (
   <Switch>
     {/*
