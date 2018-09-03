7c90bccdfe9b2873d25171fe11c010e05f942472
Jacob Anderson <odistiinct@gmail.com>
2018-09-03 11:51:08 -0500

    contact page design, and making submitting emails easy for the user

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,26 +6,39 @@ class Contact extends React.Component {
   render() {
     return (
       <div className="contact">
-        <div className="contact__contact">
-          <h4>Reason For Inquiry</h4>
-          <input />
-          <h4>First Name</h4>
-          <input />
-          <h4>Last Name</h4>
-          <input />
-          <h4>Contact Email</h4>
-          <input />
-          <h4>Contact Number</h4>
-          <input />
-          <h4>Email Contents</h4>
-          <textarea />
-          <button>Send</button>
-        </div>
+        <form
+          action="https://formspree.io/example@gmail.com"
+          method="POST"
+          className="contact__contact"
+        >
+          <h4>SUBJECT</h4>
+          <input placeholder="Business Inquiries, Hello etc." name="_subject" />
+          <h4>FULL NAME</h4>
+          <input placeholder="John Smith" type="text" name="name" />
+          <h4>CONTACT EMAIL</h4>
+          <input placeholder="john@smith.com" type="email" name="_replyto" />
+          <h4>CONTACT NUMBER</h4>
+          <input placeholder="(111) 555-1234" name="Contact_Number" />
+          <h4>YOUR MESSAGE</h4>
+          <textarea
+            placeholder="Body message..."
+            rows="6"
+            cols="32"
+            name="Email_Contents"
+          />
+          <button type="submit" value="Send">
+            Send
+          </button>
+        </form>
         <section className="contact__specific">
-          <h5>EMAIL</h5>
-          <p>example@example.io</p>
-          <h5>PHONE</h5>
-          <p>(404) 555-1234</p>
+          <div className="contact__specific__ea">
+            <h5>EMAIL</h5>
+            <p>example@example.io</p>
+          </div>
+          <div className="contact__specific__ea">
+            <h5>PHONE</h5>
+            <p>(404) 555-1234</p>
+          </div>
         </section>
       </div>
     );
